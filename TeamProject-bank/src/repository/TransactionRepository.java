fc40be1e9e64ab84ee7b753468caefd7094a8c09
sju3358 <tjdfkr011@naver.com>
2022-11-23 10:51:52 +0900

    틀 잡아봄

---
+++ TeamProject-bank/src/repository/TransactionRepository.java
@@ -0,0 +1,51 @@
+package repository;
+
+
+//거래내역
+import java.time.*;
+public class TransactionRepository{
+
+
+    private String nameOfBank;
+    private String accountNumber;
+    private long amountOfTransaction;
+    private int flagDepositOrWithdraw; //1 : deposit , -1 : withdraw;
+    private LocalDate transactionTime;
+
+    public String getNameOfBank() {
+        return nameOfBank;
+    }
+    public void setNameOfBank(String nameOfBank) {
+        this.nameOfBank = nameOfBank;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public long getAmountOfTransaction() {
+        return amountOfTransaction;
+    }
+    public void setAmountOfTransaction(long amountOfTransaction) {
+        this.amountOfTransaction = amountOfTransaction;
+    }
+
+    public int getFlagDepositOrWithdraw() {
+        return flagDepositOrWithdraw;
+    }
+    public void setFlagDepositOrWithdraw(int flagDepositOrWithdraw) {
+        this.flagDepositOrWithdraw = flagDepositOrWithdraw;
+    }
+
+    public LocalDate getTransactionTime() {
+        return transactionTime;
+    }
+    public void setTransactionTime(LocalDate transactionTime) {
+        this.transactionTime = transactionTime;
+    }
+}
+
+
