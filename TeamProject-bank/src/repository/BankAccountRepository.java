f0e139617ebddebd6dc6e76f6141d25aee3f1cf6
sju3358 <tjdfkr011@naver.com>
2022-11-23 10:23:56 +0900

    틀 잡아봄

--- TeamProject-bank/src/repository/BankAccountRepository.java
+++ TeamProject-bank/src/repository/BankAccountRepository.java
@@ -1,2 +1,10 @@
-package repository;public class BankAccountRepository {
+package repository;
+
+
+
+// 계좌 클래스
+// 소유자명, 계좌번호, 잔고로 구성
+//
+public class BankAccountRepository {
+
 }
--- TeamProject-bank/src/service/BankService.java
+++ TeamProject-bank/src/service/BankService.java
@@ -1,2 +1,14 @@
-package service;public class BankService {
+package service;
+
+
+//은행서비스
+public class BankService {
+
+    // 기능 1. 계좌번호 등록 , 계좌번호는 정규표현식으로 제한.
+    // 기능 2. 계좌 수정
+    // 기능 3. 계좌 삭제
+    // 기능 4. 계좌 검색 (소유자 명으로)
+    // 기능 5. 모든 계좌목록 조회
+    // 기능 6. 모든 거래내역 조회
+
 }
