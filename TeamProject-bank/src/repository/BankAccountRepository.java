b7ba050924bb40334f4da77333cbfc9972b554ea
2Luda <shl05234@naver.com>
2022-11-23 11:00:18 +0900

    수정

--- TeamProject-bank/src/repository/BankAccountRepository.java
+++ TeamProject-bank/src/repository/BankAccountRepository.java
@@ -6,5 +6,34 @@ package repository;
 // 소유자명, 계좌번호, 잔고로 구성
 //
 public class BankAccountRepository {
+    private String bankUseName;
+
+    public String getBankUseName() {
+        return bankUseName;
+    }
+
+    public void setBankUseName(String bankUseName) {
+        this.bankUseName = bankUseName;
+    }
+
+    public String getBankAccountNumber() {
+        return bankAccountNumber;
+    }
+
+    public void setBankAccountNumber(String bankAccountNumber) {
+        this.bankAccountNumber = bankAccountNumber;
+    }
+
+    public long getBankBalance() {
+        return bankBalance;
+    }
+
+    public void setBankBalance(long bankBalance) {
+        this.bankBalance = bankBalance;
+    }
+
+    private String bankAccountNumber;
+    private long bankBalance;
+
 
 }
