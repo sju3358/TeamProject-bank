f7ad7d3992512e97641b0fdd48a8d84d0d6bf3e5
2Luda <shl05234@naver.com>
2022-11-23 14:56:48 +0900

    수정

--- TeamProject-bank/src/Main.java
+++ TeamProject-bank/src/Main.java
@@ -1,25 +1,42 @@
+import presentation.UserInterface;
 import repository.BankAccountRepository;
 import service.BankService;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        BankService bs = new BankService();
+        BankService bankService = new BankService();
+        UserInterface UI = new UserInterface();
+        UI.run();
 
-        String bankName = "농협";
-        String bankOwner = "조성락";
-        String bankNumber = "72727272";
-        long balance = 100;
-        String password = "1234";
-        bs.addAccount(bankName,bankOwner,bankNumber,balance,password);
-        bs.listAllOfAccounts();
+        Scanner scan = new Scanner(System.in);
+
+        boolean isExit = false;
+        do{
+            int menu = Integer.parseInt(scan.nextLine());
+            switch (menu){
+                case 1 :
+                    System.out.println("개설하실 계좌의 은행,고객님의 성명,계좌번호,입금하실 금액을 알려주세요.");
+                    break;
+                case 2:
+                    System.out.println("입금 하실 계좌 번호를 알려주세요 :  " );
+
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    break;
+                case 8: isExit = true; break;
+            }
+        } while (!isExit);
 
 
-        bankName = "국민";
-        bankOwner = "조성룡";
-        bankNumber = "72327272";
-        balance = 100;
-        password = "1234";
-        bs.addAccount(bankName,bankOwner,bankNumber,balance,password);
-        bs.listAllOfAccounts();
     }
 }
