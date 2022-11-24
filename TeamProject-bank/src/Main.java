d8bf894fdd32dcfa2d013da11a217041bf4aea0b
2h5i <dev.2h5i@gmail.com>
2022-11-24 10:41:46 +0900

    수수료 출금 처리

--- TeamProject-bank/src/Main.java
+++ TeamProject-bank/src/Main.java
@@ -1,11 +1,12 @@
 
 import service.BankService;
 import presentation.UserInterface;
+
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        BankService kbBankService = new BankService("국민은행");
+        BankService kbBankService = new BankService("국민은행", 500);
         UserInterface UI = new UserInterface();
         Scanner scan = new Scanner(System.in);
 
@@ -15,39 +16,39 @@ public class Main {
 
         initAccount(kbBankService);  //init with test data
 
-        do{
-            boolean flag=false;
+        do {
+            boolean flag = false;
             UI.run();
             int menu = scan.nextInt();
             scan.nextLine();
-            switch (menu){
-                case 1 :
+            switch (menu) {
+                case 1:
                     System.out.println("고객님의 성명,계좌번호, 입금하실 금액을 알려주세요.");
                     System.out.println("고객님의 성명 : ");
                     String bankOwnerName = scan.nextLine();
-                    System.out.println("계좌번호를 입력해 주세요 :" );
+                    System.out.println("계좌번호를 입력해 주세요 :");
                     String bankAccountNumber = scan.nextLine();
-                    System.out.println("입금하실 금액을 알려주세요 :" );
+                    System.out.println("입금하실 금액을 알려주세요 :");
                     long bankBalance = scan.nextInt();
                     scan.nextLine();
                     System.out.println("비밀번호를 입력해 주세요 : ");
                     String bankPassword = scan.nextLine();
-                    kbBankService.addAccount(bankOwnerName,bankAccountNumber,bankBalance,bankPassword);
+                    kbBankService.addAccount(bankOwnerName, bankAccountNumber, bankBalance, bankPassword);
                     break;
                 case 2:
-                    System.out.println("입금하실 계좌번호를 알려주세요 :  " );
+                    System.out.println("입금하실 계좌번호를 알려주세요 :  ");
                     String depositNumber = scan.nextLine();
                     System.out.println("입금하실 금액을 적어주세요 :");
                     int depositBalance = scan.nextInt();
                     String depositPassword = "";
-                    flag = kbBankService.depositAndWithdrawMoney(depositNumber, depositBalance,depositPassword);
-                    if(flag == true)
+                    flag = kbBankService.depositAndWithdrawMoney(depositNumber, depositBalance, depositPassword);
+                    if (flag == true)
                         System.out.println("입금이 완료되었습니다.");
                     break;
                 case 3:
                     System.out.println("출금하실 계좌번호를 입력해 주세요. ");
                     String withdrawNumber = scan.nextLine();
-                    System.out.println("출금하실 금액을 적어주세요 : ");
+                    System.out.println("출금하실 금액을 적어주세요 (수수료는 " + kbBankService.getCommission() + "원 입니다.) :");
                     int withdrawBalance = scan.nextInt();
                     if (withdrawBalance > 0) {
                         withdrawBalance = withdrawBalance * -1;
@@ -56,14 +57,14 @@ public class Main {
                     System.out.println("비밀번호를 입력해주세요 :");
                     String withdrawPassword = scan.nextLine();
                     flag = kbBankService.depositAndWithdrawMoney(withdrawNumber, withdrawBalance, withdrawPassword);
-                    if(flag == true)
+                    if (flag == true)
                         System.out.println("출금이 완료되었습니다.");
                     break;
                 case 4:
                     System.out.println("계좌 수정은 1번, 계좌 삭제는 2번, 계좌 잔고 확인을 하고싶으시면 3번을 눌러주세요.");
                     int Choice = scan.nextInt();
                     scan.nextLine();
-                    if(Choice == 1) {
+                    if (Choice == 1) {
                         System.out.println("수정하실 고객님의 성명을 입력해주세요 : ");
                         String modifiedname = scan.nextLine();
                         System.out.println("수정 전 이름으로 등록되어있는 계좌번호를 입력해주세요 : ");
@@ -71,7 +72,7 @@ public class Main {
                         System.out.println("비밀번호를 입력해주세요: ");
                         String modifiedPassword = scan.nextLine();
                         flag = kbBankService.changeAccount(modifiedname, modifiedAccountNumber, modifiedPassword);
-                        if(flag){
+                        if (flag) {
                             System.out.println("수정이 완료되었습니다.");
                         }
                     } else if (Choice == 2) {
@@ -79,19 +80,19 @@ public class Main {
                         String deleteAccount = scan.nextLine();
                         System.out.println("비밀번호를 입력해주세요: ");
                         String deletePassword = scan.nextLine();
-                        flag = kbBankService.deleteAccount(deleteAccount,deletePassword);
-                        if(flag == true)
+                        flag = kbBankService.deleteAccount(deleteAccount, deletePassword);
+                        if (flag == true)
                             System.out.println("삭제되었습니다. ");
                     }
-                    if(Choice == 3) {
+                    if (Choice == 3) {
                         System.out.println("잔고 확인을 하고 싶은 계좌 번호를 입력해주세요 : ");
                         String bankAccountName = scan.nextLine();
                         System.out.println("비밀번호를 입력해주세요: ");
                         String bankAccountPassword = scan.nextLine();
 
-                        long bankMoney = kbBankService.getAccountBalance(bankAccountName,bankAccountPassword);
+                        long bankMoney = kbBankService.getAccountBalance(bankAccountName, bankAccountPassword);
                         if (bankMoney > 0) {
-                            System.out.println("현재 잔액 :" + bankMoney );
+                            System.out.println("현재 잔액 :" + bankMoney);
                         }
                     }
                     break;
@@ -99,13 +100,13 @@ public class Main {
                     System.out.println("이름으로 계좌 조회를 하고싶으시면 1번, 계좌 번호로 계좌 조회를 하고싶으시면 2번을 눌러주세요.");
                     int searchChoice = scan.nextInt();
                     scan.nextLine();
-                    if(searchChoice == 1){
+                    if (searchChoice == 1) {
                         System.out.println("이름을 입력해주세요 : ");
                         String nameSearch = scan.nextLine();
                         System.out.println("조회 결과: ");
                         kbBankService.searchAccountByName(nameSearch);
                     }
-                    if(searchChoice == 2){
+                    if (searchChoice == 2) {
                         System.out.println("계좌 번호를 입력해주세요 : ");
                         String numberSearch = scan.nextLine();
                         System.out.println("조회 결과: ");
@@ -124,20 +125,21 @@ public class Main {
                     break;
                 case 8:
                     System.out.println("이용해 주셔서 감사합니다.");
-                    isExit = true; break;
+                    isExit = true;
+                    break;
             }
         } while (!isExit);
 
 
     }
 
-    private static void initAccount(BankService kbBankService){
-        String[] bankOwnerName = {"조성락","곽두영","김태이","배지호","김지환"};
-        String[] bankAccountNumber = {"111-111-1111","222-222-2222","333-333-3333","444-444-4444","555-555-5555"};
-        long [] bankBalance = {1000000,5000,100000,0,100};
-        String[] bankPassword = {"1212","3030","2412","4039","1241"};
+    private static void initAccount(BankService kbBankService) {
+        String[] bankOwnerName = {"조성락", "곽두영", "김태이", "배지호", "김지환"};
+        String[] bankAccountNumber = {"111-111-1111", "222-222-2222", "333-333-3333", "444-444-4444", "555-555-5555"};
+        long[] bankBalance = {1000000, 5000, 100000, 0, 100};
+        String[] bankPassword = {"1212", "3030", "2412", "4039", "1241"};
 
-        for(int i = 0; i<bankAccountNumber.length; i++)
-            kbBankService.addAccount(bankOwnerName[i],bankAccountNumber[i],bankBalance[i],bankPassword[i]);
+        for (int i = 0; i < bankAccountNumber.length; i++)
+            kbBankService.addAccount(bankOwnerName[i], bankAccountNumber[i], bankBalance[i], bankPassword[i]);
     }
 }
--- TeamProject-bank/src/service/BankService.java
+++ TeamProject-bank/src/service/BankService.java
@@ -5,39 +5,46 @@ import java.time.LocalDateTime;
 //은행서비스
 public class BankService {
 
-    private String bankName;
-
+    private final String bankName;
+    private final int commission;
     private TransactionHistoryService transactionHistoryService;
     private BankAccountService bankAccountService;
 
-    public BankService(String bankName){
+
+    public BankService(String bankName, int commission) {
         this.bankName = bankName;
+        this.commission = commission;
         transactionHistoryService = TransactionHistoryService.getInstance();
         bankAccountService = BankAccountService.getInstance();
     }
 
+    public int getCommission() {
+        return commission;
+    }
 
     /**
      * 계좌 등록 메소드
-     * @param bankOwnerName (String)
+     *
+     * @param bankOwnerName     (String)
      * @param bankAccountNumber (String)
-     * @param bankBalance (long)
+     * @param bankBalance       (long)
      */
-    public void addAccount(String bankOwnerName, String bankAccountNumber, long bankBalance, String password){
-        bankAccountService.addAccount(this.bankName,bankOwnerName,bankAccountNumber,bankBalance,password);
+    public void addAccount(String bankOwnerName, String bankAccountNumber, long bankBalance, String password) {
+        bankAccountService.addAccount(this.bankName, bankOwnerName, bankAccountNumber, bankBalance, password);
     }
 
     /**
      * 계좌 수정 메소드
-     * @param bankOwnerName (String)
+     *
+     * @param bankOwnerName     (String)
      * @param bankAccountNumber (String)
-     * @param password (String)
+     * @param password          (String)
      */
-    public boolean changeAccount(String bankOwnerName, String bankAccountNumber, String password){
+    public boolean changeAccount(String bankOwnerName, String bankAccountNumber, String password) {
 
-        boolean flag = bankAccountService.modifyAccount(this.bankName,bankOwnerName,bankAccountNumber,password);
+        boolean flag = bankAccountService.modifyAccount(this.bankName, bankOwnerName, bankAccountNumber, password);
 
-        if(flag== true)
+        if (flag == true)
             return true;
         else
             return false;
@@ -45,12 +52,13 @@ public class BankService {
 
     /**
      * 계좌 삭제 메소드
+     *
      * @param bankAccountNumber (String)
      */
-    public boolean deleteAccount(String bankAccountNumber, String password){
+    public boolean deleteAccount(String bankAccountNumber, String password) {
 
         boolean flag = bankAccountService.deleteAccount(bankAccountNumber, password);
-        if(flag== true)
+        if (flag == true)
             return true;
         else
             return false;
@@ -59,42 +67,50 @@ public class BankService {
 
     /**
      * 잔액 조회 메소드
+     *
      * @param bankAccountNumber (String)
-     *  @param password (String)
+     * @param password          (String)
      */
-    public long getAccountBalance(String bankAccountNumber, String password){
+    public long getAccountBalance(String bankAccountNumber, String password) {
         return bankAccountService.getAccountBalance(bankAccountNumber, password);
     }
 
     /**
      * 계좌 이름으로 검색 메소드
+     *
      * @param name (String)
      */
-    public void searchAccountByName(String name){
+    public void searchAccountByName(String name) {
         System.out.println(bankAccountService.getStringFromListOfAccount(bankAccountService.getAccountsByName(name)));
     }
 
     /**
      * 계좌 번호로 검색 메소드
+     *
      * @param bankAccountNumber
      */
-    public void searchAccountByNumber(String bankAccountNumber){
-        if(bankAccountService.getAccountsByNumber(bankAccountNumber) != null)
+    public void searchAccountByNumber(String bankAccountNumber) {
+        if (bankAccountService.getAccountsByNumber(bankAccountNumber) != null)
             System.out.println(bankAccountService.getAccountsByNumber(bankAccountNumber));
     }
 
     /**
      * 계좌 입출금 메소드
+     *
      * @param BankAccountNumber (String)
-     * @param amount (int)
-     * @param password (String)
+     * @param amount            (int)
+     * @param password          (String)
      */
-    public boolean depositAndWithdrawMoney(String BankAccountNumber, int amount , String password){
+    public boolean depositAndWithdrawMoney(String BankAccountNumber, int amount, String password) {
 
         boolean flag = false;
-        flag = bankAccountService.depositAndWithdraw(BankAccountNumber,amount, password);
+        if (amount < 0) {
+            flag = bankAccountService.depositAndWithdraw(BankAccountNumber, amount - this.commission, password);
+        } else {
+            flag = bankAccountService.depositAndWithdraw(BankAccountNumber, amount, password);
+        }
 
-        if(flag == true) {
+        if (flag == true) {
             //잔고 변동시 트렌젝션 기록
             LocalDateTime date = LocalDateTime.now();
             transactionHistoryService.addTransaction(
@@ -114,14 +130,14 @@ public class BankService {
     /**
      * 모든 계좌 리스트
      */
-    public void listAllOfAccounts(){
+    public void listAllOfAccounts() {
         System.out.println(bankAccountService.getListOfAccounts());
     }
 
     /**
      * 모든 거래내역 리스트
      */
-    public void listAllOfTransactions(){
+    public void listAllOfTransactions() {
         System.out.println(transactionHistoryService.getListOfTransactions());
     }
 
